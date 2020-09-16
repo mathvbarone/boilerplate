dc7da1a9fef05b329b1f8da0ce952d1097bf4870
Matheus <matheus.barone@raisin.com>
2020-09-16 14:58:59 +0200

    Update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,4 @@
 module.exports = {
-  stories: [
-    '../src/components/**/stories.mdx',
-    '../src/components/**/stories.@(js|jsx|ts|tsx)'
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs'
-  ]
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials']
 }
